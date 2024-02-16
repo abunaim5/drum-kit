2907c7d4cf7ed10972b9799419e395580c658c6f
abunaim5 <abunaim140@gmail.com>
2024-02-16 16:04:17 +0600

    added some changes

--- index.js
+++
@@ -1,7 +0,0 @@
-// window.addEventListener('keydown', function(KeyboardEvent) {
-//     const audio = this.document.querySelector(KeyboardEvent.key)
-//     if(!audio){
-//         return;
-//     }
-//     audio.play();
-// });
